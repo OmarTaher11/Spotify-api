96cbbaba57b752549bb89d1c15a170d1ebea04b1
Michael <michaelsoliman981@gmail.com>
2020-04-23 13:47:21 +0200

    last version after-integration code

--- src/spotify-api.js
+++ src/spotify-api.js
@@ -1,11 +1,16 @@
 const express = require('express')
- require('../src/db/mongoose')
+const cors = require("cors")
+
+require('../src/db/mongoose')
 const userRouter = require('./routers/user')
 const followRouter = require('../src/routers/follower')
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use(userRouter)
 app.use(followRouter)
-app.listen(3000,()=>{
+
+
+app.listen(3003,()=>{
     console.log("server is up")
 })
